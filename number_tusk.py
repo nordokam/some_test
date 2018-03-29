78f82a7706ba5f2cdc8596dad358b29c830096ba
gugry <gugry@mail.ru>
2018-03-29 10:19:57 +1000

    .

--- number_tusk.py
+++ number_tusk.py
@@ -2,6 +2,6 @@ import sys
 
 if len(sys.argv)>1:
     n = int(sys.argv[1])
-    print(str((n // 60) % 24, n % 60))
+    print('{}{}'.format((n // 60) % 24 , n % 60))
 else:
-    print('err')
+    print(sys.argv)
